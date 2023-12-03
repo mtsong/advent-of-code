46e3d1e29ecdb73223e765b77dc1ba0fb1946a1b
Michael Song <msong@teamworks.com>
2023-12-03 14:26:07 -0500

    Day 3 part 2 solution

--- 3/answer.py
+++ 3/answer.py
@@ -40,28 +40,37 @@ with open(f"{DAY}/example.txt" if USE_EXAMPLE else f"{DAY}/input.txt", encoding=
 
 tokenized_data = list(tokenize(data))
 
-# Get symbols for each line
-symbol_by_line = defaultdict(list)
+# Get numbers for each line
+numbers_by_line = defaultdict(list)
 for token in tokenized_data:
-    if token.type == "SYMBOL":
-        symbol_by_line[token.line].append(token)
+    if token.type == "NUMBER":
+        numbers_by_line[token.line].append(token)
 
 # For each number, see if there's a symbol in matching range and add to answer if so
 answer = 0
+num_gears = 0
 for token in tokenized_data:
-    if token.type == "NUMBER":
+    if token.value == "*":
         start_line = token.line - 1 if token.line > 1 else 1
         end_line = token.line + 1
         start_column = token.column - 1 if token.column > 0 else 0
-        end_column = token.column + len(str(token.value))
+        end_column = token.column + 1
+        adjacent_numbers = []
         for line in range(start_line, end_line + 1):
-            part_valid = False
-            for symbol in symbol_by_line[line]:
-                if start_column <= symbol.column <= end_column:
-                    answer += token.value
-                    print(f"Valid part: {token.value}")
-                    part_valid = True
-                    break
-            if part_valid:
-                break
+            for number in numbers_by_line[line]:
+                number_width = len(str(number.value))
+                number_end_column = number.column + number_width - 1
+                if (
+                    token.column == number.column
+                    or number.column <= start_column <= number_end_column
+                    or number.column <= end_column <= number_end_column
+                ):
+                    adjacent_numbers.append(number)
+        if len(adjacent_numbers) == 2:
+            # print(f"Gear found at {token.line},{token.column} with part numbers {[a.value for a in adjacent_numbers]}")
+            num_gears += 1
+            answer += adjacent_numbers[0].value * adjacent_numbers[1].value
+        else:
+            print(f"No gear found at {token.line},{token.column}")
+print(num_gears)
 print(answer)
