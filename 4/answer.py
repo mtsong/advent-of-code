52f29e1d41f2c38d19ff7023b7e00a392e67b0f4
Michael Song <msong@teamworks.com>
2023-12-04 09:36:52 -0500

    Day 4 part 2 example answer

--- 4/answer.py
+++ 4/answer.py
@@ -1,24 +1,39 @@
 DAY = 4
-USE_EXAMPLE = False
+USE_EXAMPLE = True
 
 with open(f"{DAY}/example.txt" if USE_EXAMPLE else f"{DAY}/input.txt", encoding="utf-8") as f:
     lines = f.readlines()
 
 answer = 0
+cards = []
 for line in lines:
     card_info, number_info = line.split(":")
-    card = card_info.split()[1]
+    card_num = card_info.split()[1]
     numbers = number_info.split("|")
     winning_numbers = {int(n) for n in numbers[0].split()}
     numbers = {int(n) for n in numbers[1].split()}
+    cards.append((card_num, winning_numbers, numbers))
+
+winning_copies = []
+
+
+def copy_winning_cards(card):
+    card_num, winning_numbers, numbers = card
     wins = numbers & winning_numbers
-    points = 0
-    if wins:
-        points += pow(2, len(wins) - 1)
-        print(
-            f"Card {card} has {len(wins)} winning numbers ({", ".join(str(w) for w in wins)}), so it is worth {points} points."
-        )
-    else:
-        print(f"Card {card} has no winning numbers, so it is worth no points.")
-    answer += points
+    if len(wins) == 0:
+        return
+    copies = cards[int(card_num) : int(card_num) + len(wins)]
+    winning_copies.extend(copies)
+    for copy in copies:
+        copy_winning_cards(copy)
+
+
+for card in cards:
+    copy_winning_cards(card)
+
+winning_cards = cards + winning_copies
+winning_cards.sort(key=lambda card: int(card[0]))
+for card in winning_cards:
+    _, winning_numbers, numbers = card
+    answer += pow(2, len(numbers & winning_numbers) + 1)
 print(f"The Elf's pile of scratchcards is worth {answer} points.")
