6aff07846f0502ade10263f67f8a3d899ab11b04
Michael Song <msong@teamworks.com>
2023-12-05 11:57:18 -0500

    Brute force solution for day 5 part 2

--- 5/answer.py
+++ 5/answer.py
@@ -1,12 +1,17 @@
 import pathlib
+import sys
 
-USE_EXAMPLE = False
+USE_EXAMPLE = True
 CWD = pathlib.Path(__file__).parent.resolve()
 with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8") as f:
     lines = f.read()
 
 
-def parse_map(map):
+def range_intersect(range1: range, range2: range) -> range | None:
+    return range(max(range1.start, range2.start), min(range1.stop, range2.stop)) or None
+
+
+def parse_map(map: str) -> list[tuple[int, int, int]]:
     special_mappings = []
     for line in map.split("\n"):
         if not line:
@@ -20,31 +25,37 @@ def parse_map(map):
 
 
 sections = lines.split("\n\n")
-seeds = [int(seed) for seed in sections[0].split(":")[1].split()]
+seeds = []
+seed_ranges = sections[0].split(":")[1].split()
+for i, seed_range in enumerate(seed_ranges):
+    if i % 2 == 0:
+        seeds.append((range(int(seed_range), int(seed_range) + int(seed_ranges[i + 1]))))
+print(f"Found {sum(len(seed) for seed in seeds)} seeds")
+
 maps = []
 for section in sections[1:]:
     name, map = section.split(":")
     maps.append((name.split()[0].split("-")[2], parse_map(map)))
 
-locations = []
-next_lookup = 0
-for seed in seeds:
-    next_lookup = seed
-    route = []
-    category = "seed"
-    for category, map in maps:
-        special_ranges = []
-        for map_range in map:
-            dest_start, source_start, range_len = map_range
-            source_range = range(source_start, source_start + range_len)
-            dest_range = range(dest_start, dest_start + range_len)
-            special_ranges.append((source_range, dest_range))
+min_location = sys.maxsize
+for seed_range in seeds:
+    for key in seed_range:
+        route = []
+        category = "seed"
+        for category, map in maps:
+            special_ranges = []
+            for map_range in map:
+                dest_start, source_start, range_len = map_range
+                source_range = range(source_start, source_start + range_len)
+                dest_range = range(dest_start, dest_start + range_len)
+                special_ranges.append((source_range, dest_range))
 
-        for special_range in special_ranges:
-            if next_lookup in special_range[0]:
-                next_lookup = special_range[1][next_lookup - special_range[0][0]]
-                break
-        route.append(f"{category} {next_lookup}")
-    locations.append(next_lookup)
-    print(", ".join(route))
-print(f"The lowest location number is {min(locations)}")
+            for special_range in special_ranges:
+                if key in special_range[0]:
+                    key = special_range[1][key - special_range[0][0]]
+                    break
+            route.append(f"{category} {key}")
+        if key < min_location:
+            min_location = key
+        print(", ".join(route))
+print(f"The lowest location number is {min_location}")
