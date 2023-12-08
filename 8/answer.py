38a2102484183bdefa8c0dbd78c4e2e690761209
Michael Song <msong@teamworks.com>
2023-12-08 17:49:50 -0500

    Day 8 part 2 solution

--- 8/answer.py
+++ 8/answer.py
@@ -1,4 +1,5 @@
 import pathlib
+import math
 
 USE_EXAMPLE = False
 CWD = pathlib.Path(__file__).parent.resolve()
@@ -15,11 +16,17 @@ with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8
             node, elements = line.split("=")
             nodes[node.strip()] = tuple(e.strip() for e in elements.strip("\n ()").split(","))
 
-steps = 0
-element = "AAA"
-while element != "ZZZ":
-    print(element)
-    instruction = instructions[steps % len(instructions)]
-    element = nodes[element][instruction == "R"]
-    steps += 1
-print(f"{steps} steps required to go from AAA to ZZZ")
+element_steps = []
+elements = tuple(node for node in nodes.keys() if node.endswith("A"))
+dest_elements = []
+for element in elements:
+    steps = 0
+    orig_element = element
+    while not element.endswith("Z"):
+        instruction = instructions[steps % len(instructions)]
+        element = nodes[element][instruction == "R"]
+        steps += 1
+    dest_elements.append(element)
+    print(f"{steps} steps required to go from {orig_element} to {element}")
+    element_steps.append(steps)
+print(f"{math.lcm(*element_steps)} steps required to go from {elements} to {tuple(dest_elements)}")
