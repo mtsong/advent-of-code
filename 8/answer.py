24c4df59633c61bca2532b89b30155683c2f3a91
Michael Song <msong@teamworks.com>
2023-12-08 17:28:57 -0500

    Day 8 Part 1 solution

--- 8/answer.py
+++ 8/answer.py
@@ -1,6 +1,6 @@
 import pathlib
 
-USE_EXAMPLE = True
+USE_EXAMPLE = False
 CWD = pathlib.Path(__file__).parent.resolve()
 nodes = {}
 with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8") as f:
