a501bca7600fbf648f96e41b3e39aaf64ec82a67
Michael Song <msong@teamworks.com>
2023-12-09 09:03:34 -0500

    Day 9 part 2 solution

--- 9/answer.py
+++ 9/answer.py
@@ -1,34 +1,28 @@
 import pathlib
 
-USE_EXAMPLE = True
+USE_EXAMPLE = False
 CWD = pathlib.Path(__file__).parent.resolve()
 with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8") as f:
     histories = []
     for line in f:
         histories.append([int(n) for n in line.split()])
 
-total_next_value = 0
+total_previous_values = 0
 for history in histories:
     original_history = history
     sequences = [history]
-    found = False
-    while history and not found:
+    while set(history) != {0}:
         print(history)
         diffs = []
-        reversed_history = list(reversed(history))
-        for i, value in enumerate(reversed_history):
-            if i < len(reversed_history) - 1:
-                diff_with_previous = value - reversed_history[i + 1]
-                if i == 1 and diff_with_previous == diffs[i - 1]:
-                    next_value = diff_with_previous
-                    while sequences:
-                        next_value = sequences.pop()[-1] + next_value
-                    total_next_value += next_value
-                    found = True
-                    break
-                diffs.append(diff_with_previous)
+        for i, value in enumerate(history):
+            if i < len(history) - 1:
+                diffs.append(history[i + 1] - value)
         else:
-            history = list(reversed(diffs))
+            history = diffs
             sequences.append(history)
-    print(f"The next value of {original_history} is {next_value}")
-print(f"The sum of these extrapolated values is {total_next_value}")
+    previous_value = 0
+    while sequences:
+        previous_value = sequences.pop()[0] - previous_value
+    total_previous_values += previous_value
+    print(f"The previous value of {original_history} is {previous_value}")
+print(f"The sum of these extrapolated values is {total_previous_values}")
