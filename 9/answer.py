29619bfc0a5926f2f7fcc918e4c930eb48bd6b85
Michael Song <msong@teamworks.com>
2023-12-09 08:57:15 -0500

    Non-working solution for Day 9 part 1

--- 9/answer.py
+++ 9/answer.py
@@ -9,6 +9,7 @@ with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8
 
 total_next_value = 0
 for history in histories:
+    original_history = history
     sequences = [history]
     found = False
     while history and not found:
@@ -19,7 +20,7 @@ for history in histories:
             if i < len(reversed_history) - 1:
                 diff_with_previous = value - reversed_history[i + 1]
                 if i == 1 and diff_with_previous == diffs[i - 1]:
-                    next_value = abs(diff_with_previous)
+                    next_value = diff_with_previous
                     while sequences:
                         next_value = sequences.pop()[-1] + next_value
                     total_next_value += next_value
@@ -29,5 +30,5 @@ for history in histories:
         else:
             history = list(reversed(diffs))
             sequences.append(history)
-    print(f"The next value of {history} is {next_value}")
+    print(f"The next value of {original_history} is {next_value}")
 print(f"The sum of these extrapolated values is {total_next_value}")
