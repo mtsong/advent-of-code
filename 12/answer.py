3a1b12206971a0b53d227140afee9f885081fae3
Michael Song <msong@teamworks.com>
2023-12-12 09:44:24 -0500

    Solution with generating string from regex

--- 12/answer.py
+++ 12/answer.py
@@ -1,8 +1,10 @@
 import pathlib
 import re
+import itertools
+import exrex
 
 
-USE_EXAMPLE = False
+USE_EXAMPLE = True
 CWD = pathlib.Path(__file__).parent.resolve()
 with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8") as f:
     lines = [line.strip().split() for line in f]
@@ -22,10 +24,21 @@ def arrange(springs):
 
 total_arrangements = 0
 for springs, config in lines:
-    arrangements = list(arrange(springs))
-    pattern = re.compile(rf"^\.*{r"\.+".join([f"#{{{c}}}" for c in config.split(",")])}\.*$")
-    print(f"Checking {len(arrangements)} arrangements for {springs} and {config}")
-    for arrangement in arrangements:
-        if is_valid(arrangement, pattern):
-            total_arrangements += 1
+    # springs = "?".join(itertools.repeat(springs, 5))
+    config = config.split(",")
+    leading_dots = len(springs) - len(springs.lstrip("."))
+    trailing_dots = len(springs) - len(springs.rstrip("."))
+    valid_arrangements = list(
+        exrex.generate(
+            rf"^\.{{{leading_dots}}}{r"\.+".join([f"#{{{c}}}" for c in config])}\.{{{trailing_dots}}}$",
+            1,
+        )
+    )
+    if len(valid_arrangements) == 1:
+        print(f"The only valid arrangement for {springs} and {config} is:")
+        print(f"{valid_arrangements[0]}\n")
+        total_arrangements += 1
+    else:
+        print(f"There are {len(valid_arrangements)} possible arrangements for {springs} and {config}\n")
+        total_arrangements += len(valid_arrangements)
 print(f"The number of possible arrangements is: {total_arrangements}")
