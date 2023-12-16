4b369672818677cd8156fe451414ef04243d90d4
Michael Song <michael.song1991@gmail.com>
2023-12-15 20:52:35 -0500

    Day 14 part 2 WIP

--- 2023/14/answer.py
+++ 2023/14/answer.py
@@ -2,7 +2,7 @@ import pathlib
 
 import numpy as np
 
-USE_EXAMPLE = False
+USE_EXAMPLE = True
 CWD = pathlib.Path(__file__).parent.resolve()
 with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8") as f:
     rows = []
@@ -10,29 +10,32 @@ with open(CWD / ("example.txt" if USE_EXAMPLE else "input.txt"), encoding="utf-8
         rows.append(list(line.strip()))
     dish = np.array(rows)
 
-total_load = 0
+# Tilt North
 for c, column in enumerate(dish.T):
-    load = 0
     square_rocks = []  # Indexes
     round_rocks = []  # Indexes
-    for i, p in enumerate(reversed(column)):
+    for i, p in enumerate(column):
         if p == "#":
-            square_rocks.append(i + 1)
+            square_rocks.append(i)
         elif p == "O":
-            round_rocks.append(i + 1)
-    if round_rocks and not square_rocks or square_rocks and round_rocks and max(square_rocks) < min(round_rocks):
-        load = (len(column) + len(column) - len(round_rocks) + 1) * len(round_rocks) // 2
-        print(f"1. The load applied by column {c + 1} is {load}")
-    else:
-        while square_rocks and round_rocks:
-            while square_rocks and (square_rock := square_rocks.pop(0)) < round_rocks[0]:
-                pass
-            blocked_rounds = []
-            while round_rocks and (round_rock := round_rocks[0]) < square_rock:
-                blocked_rounds.append(round_rocks.pop(0))
-            load += (square_rock - 1 + square_rock - len(blocked_rounds)) * len(blocked_rounds) // 2
-        if round_rocks:
-            load += (len(column) + len(column) - len(round_rocks) + 1) * len(round_rocks) // 2
-        print(f"2. The load applied by column {c + 1} is {load}")
-    total_load += load
-print(f"The total load on the north support beams is {total_load}")
+            round_rocks.append(i)
+    while round_rocks:
+        square_rock = len(column)
+        while square_rocks and (square_rock := square_rocks.pop(0)) > round_rocks[0]:
+            pass
+        if not square_rocks and square_rock > round_rocks[-1]:
+            for j, round_rock in enumerate(round_rocks):
+                column[round_rock] = "."
+                column[j] = "O"
+            break
+        blocked_rounds = []
+        while round_rocks and (round_rock := round_rocks[0]) >= square_rock:
+            blocked_rounds.append(round_rocks.pop(0))
+        if not blocked_rounds:
+            blocked_rounds = round_rocks
+            round_rocks = []
+        for j, blocked_round in enumerate(blocked_rounds):
+            column[blocked_round] = "."
+            column[j] = "O"
+print(dish)
+print(f"The total load on the north support beams is {0}")
